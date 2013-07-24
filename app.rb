507cc88fd9f8329160ee7efb99af6bf862ee7306
Zee Spencer <zee@zeespencer.com>
2013-07-24 14:39:15 -0400

    Extract views to individual files

--- app.rb
+++ app.rb
@@ -17,8 +17,6 @@ post '/charge' do
   # Amount in cents
   @amount = 10000
 
-  puts params
-
   customer = Stripe::Customer.create(
     :email => params[:email],
     :card  => params[:stripeToken]
@@ -38,33 +36,3 @@ end
 error Stripe::CardError do
   env['sinatra.error'].message
 end
-
-__END__
-
-@@ layout
-<!DOCTYPE html>
-<html>
-<head></head>
-<body>
-<%= yield %>
-</body>
-</html>
-
-
-@@ index
-  <form action="/charge" method="post" class="payment">
-    <article>
-      <label class="amount">
-        <span>Amount: $100.00</span>
-      </label>
-      <label>Email:
-        <input name="email" type="email">
-      </label>`
-    </article>
-
-    <script src="https://checkout.stripe.com/v2/checkout.js" class="stripe-button"
-            data-key="<%= settings.publishable_key %>"></script>
-  </form>
-
-@@ charge
-  <h2>Thanks, you paid <strong>$100.00</strong>!</h2>
